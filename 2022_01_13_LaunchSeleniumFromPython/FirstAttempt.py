b10a717ced047769d6f189a57bdc3d596b148028
Éloi Strée <eloistree@gmail.com>
2022-01-13 16:47:07 +0100

    Create FirstAttempt.py

---
+++ 2022_01_13_LaunchSeleniumFromPython/FirstAttempt.py
@@ -0,0 +1,12 @@
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver=webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://www.twitch.tv/popout/openmacroinput/chat")
+print("Application title is ", driver.title)
+print("Application url is ", driver.current_url)
+
+#sleep(500)
+#driver.quit()
